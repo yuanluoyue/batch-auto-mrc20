04cbb54421176e008674ed6065ce074434759668
noname <yeunghuanyuan@qq.com>
2024-01-07 21:42:56 +0800

    fix: record id

--- lib.js
+++ lib.js
@@ -1,10 +1,12 @@
+const config = require('./config')
+
 async function sleepMS(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 async function get_current_epoch(suiClient) {
   const tick_record = await suiClient.getObject({
-    id: TickRecordID,
+    id: config.recordID,
     options: { showContent: true, showDisplay: true },
   })
   return parseInt(tick_record.data.content.fields.current_epoch)
